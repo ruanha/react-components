ef6dec36e06a80048493d8d813b74483e070f268
Rune Hartvig <rune_hartvig@hotmail.com>
2023-01-27 18:56:07 +0100

    add progress bar component

--- src/components/progress-bar/progress-bar.tsx
+++ src/components/progress-bar/progress-bar.tsx
@@ -1,3 +1,25 @@
-export default function ProgressBar() {
-  return <h1>PROGRESS BAR</h1>;
+import { CSSProperties } from "react";
+import "./progress-bar.css";
+
+export default function ProgressBar({ style, percent = 0 }: props) {
+  percent = percent < 0 ? 0 : percent;
+  percent = percent > 100 ? 100 : percent;
+
+  return (
+    <div className="progress-bar" style={{ ...style?.container }}>
+      <div className="progress" style={{ ...style?.bar, width: `${percent}%` }}>
+        {percent}%
+      </div>
+    </div>
+  );
 }
+
+type styleProp = {
+  bar?: CSSProperties;
+  container?: CSSProperties;
+};
+
+type props = {
+  style?: styleProp;
+  percent?: number;
+};
