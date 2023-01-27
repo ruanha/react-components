666af27457b0b59e2b855282343c86f52276c494
Rune Hartvig <rune_hartvig@hotmail.com>
2023-01-27 12:05:40 +0100

    add links to dummy content in nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useState, MouseEvent } from "react";
+
 import "./App.css";
 
 function App() {
+  const [selectedComponent, setSelectedComponent] = useState("");
+
+  const handleComponentClick = (event: MouseEvent<HTMLAnchorElement>) => {
+    const component = event.currentTarget
+      .getAttribute("href")
+      ?.replace("#", "");
+    setSelectedComponent(component ?? "");
+  };
+
   return (
     <div className="app">
       <header>
@@ -9,9 +20,27 @@ function App() {
       <div className="columns">
         <nav>
           <h2>NAVIGATION</h2>
+          <ul>
+            <li>
+              <a href="#progress-bar" onClick={handleComponentClick}>
+                Progress Bar
+              </a>
+            </li>
+            <li>
+              <a href="#accordion" onClick={handleComponentClick}>
+                Accordion
+              </a>
+            </li>
+            <li>
+              <a href="#button" onClick={handleComponentClick}>
+                Button
+              </a>
+            </li>
+          </ul>
         </nav>
         <main>
           <h2>MAIN CONTENT</h2>
+          {selectedComponent}
         </main>
         <aside>
           <h2>ASIDE</h2>
