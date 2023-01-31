71ca9901d816131b7b94dbef62aceacd321a7ffd
Rune Hartvig <rune_hartvig@hotmail.com>
2023-01-31 20:22:27 +0100

    test status change after successful like

--- src/components/like-button/like-button.test.tsx
+++ src/components/like-button/like-button.test.tsx
@@ -1,16 +1,34 @@
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import LikeButton from "./like-button";
 
 const url = "/mock/api/like";
 
-test("renders Like button in unliked state as default", () => {
-  render(<LikeButton url={url} requestApi={mockFetch} />);
-  const linkElement = screen.getByText(/Like/i);
-  expect(linkElement).toBeInTheDocument();
+// afterEach function runs after each test suite is executed
+afterEach(() => {
+  cleanup();
 });
 
-function mockFetch(_url: string, _options: any): Promise<any> {
-  return new Promise((resolve, reject) => {
+test("renders Like button in unliked state as default", () => {
+  render(<LikeButton url={url} requestApi={mockFetch} />);
+  const buttonElement = screen.getByRole("button");
+  expect(buttonElement).toHaveTextContent("Like");
+});
+
+test("renders a Liked button when fetch is successful", async () => {
+  // arrange
+  render(<LikeButton url={url} requestApi={mockFetch} />);
+  const buttonElement = screen.getByRole("button");
+
+  // act
+  userEvent.click(buttonElement);
+
+  // assert
+  await waitFor(() => expect(buttonElement).toHaveTextContent("Liked"));
+});
+
+async function mockFetch(_url: string, _options: any): Promise<any> {
+  return new Promise((resolve, _reject) => {
     resolve({
       ok: true,
     });
