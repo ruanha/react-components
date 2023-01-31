ae1b23b4f3c4f47f5245adf66f074f7142d68d1f
Rune Hartvig <rune_hartvig@hotmail.com>
2023-01-31 10:42:26 +0100

    left align and adjust font-size of code highlight

--- src/components-examples/progress-bar.tsx
+++ src/components-examples/progress-bar.tsx
@@ -11,17 +11,24 @@ export default function ProgressBarExample() {
         If no style prop is used, the progress bar uses the defaults. If no
         percentage is used it defaults to 0%.
       </p>
-      <SyntaxHighlighter language="javascript" style={dracula}>
-        {`
-                import ProgressBar from "./portfolio/progress_bar/progress-bar";
-                <ProgressBar percent={0} />`}
-      </SyntaxHighlighter>
+
       <ProgressBar percent={0} />
       <ProgressBar percent={1} />
       <ProgressBar percent={5} />
       <ProgressBar percent={25} />
       <ProgressBar percent={50} />
       <ProgressBar percent={100} />
+
+      <div className="example-code">
+        <SyntaxHighlighter
+          language="javascript"
+          style={dracula}
+          customStyle={{ fontSize: "0.7rem" }}
+        >
+          {codeExample_1}
+        </SyntaxHighlighter>
+      </div>
+
       <h4>Style the progress bar</h4>
       <p>
         The style of the progress bar, text and container is configurable with
@@ -51,3 +58,13 @@ export default function ProgressBarExample() {
     </div>
   );
 }
+
+const codeExample_1 = `import ProgressBar from "./portfolio/progress_bar/progress-bar";
+
+<ProgressBar percent={0} />
+<ProgressBar percent={0} />
+<ProgressBar percent={1} />
+<ProgressBar percent={5} />
+<ProgressBar percent={25} />
+<ProgressBar percent={50} />
+<ProgressBar percent={100} />`;
