1da906afee5f628fe47bcc88f06b36f0ee15b109
Rune Hartvig <rune_hartvig@hotmail.com>
2023-01-31 21:26:00 +0100

    test error message

--- src/components/like-button/like-button.test.tsx
+++ src/components/like-button/like-button.test.tsx
@@ -38,15 +38,46 @@ test("Like button toggles between Like and Liked", async () => {
   await userEvent.click(buttonElement);
 
   // assert
-  expect.assertions(2);
   await waitFor(() => expect(buttonElement).toHaveTextContent(/^Like$/));
-  expect(buttonElement).not.toHaveTextContent(/^Liked$/);
+});
+
+test("does not change status if request rejects", async () => {
+  // arrange
+  render(<LikeButton url={url} requestApi={mockFetchFail} />);
+  const buttonElement = screen.getByRole("button");
+
+  // act
+  await userEvent.click(buttonElement);
+
+  // assert
+  await waitFor(() => expect(buttonElement).toHaveTextContent(/^Like$/));
+});
+
+test("contains error message if request rejects", async () => {
+  // arrange
+  render(<LikeButton url={url} requestApi={mockFetchFail} />);
+  const buttonElement = screen.getByRole("button");
+
+  // act
+  await userEvent.click(buttonElement);
+
+  // assert
+  const errorElement = await screen.findByText(/Something went wrong/);
+  expect(errorElement).toBeInTheDocument();
 });
 
 async function mockFetch(_url: string, _options: any): Promise<any> {
   return new Promise((resolve, _reject) => {
     resolve({
       ok: true,
+    });
+  });
+}
+
+async function mockFetchFail(_url: string, _options: any): Promise<any> {
+  return new Promise((_resolve, reject) => {
+    reject({
+      error: "Something went wrong",
     });
   });
 }
